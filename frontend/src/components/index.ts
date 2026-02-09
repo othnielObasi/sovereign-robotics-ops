7846a6cfabc3e2eaa04a1532a0c29cda21adfad2
Othniel Obasi <122861704+othnielObasi@users.noreply.github.com>
2026-02-09 15:50:56 +0000

    Delete frontend directory

--- frontend/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Map2D } from './Map2D';
-
-export { default as Map2DEnhanced } from './Map2DEnhanced';
-export { default as Timeline } from './Timeline';
-export { default as AlertsPanel } from './AlertsPanel';
